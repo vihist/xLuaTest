8b7134fb0973c0283b81c23d501b8217ab87b63b
vihist <vihist@outlook.com>
2018-03-01 17:32:55 +0800

    1 lua move to StreamingAssets 2 change lua struct as table

--- Assets/Script/MainScript.cs
+++ Assets/Script/MainScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using XLua;
+using System.IO;
 
 [LuaCallCSharp]
 public class TestClass
@@ -13,39 +14,71 @@ public class TestClass
 	}
 }
 
+[CSharpCallLua]
+public interface ItfOption
+{
+	string op1 { get; }
+	string op2 { get; }
+	string op3 { get; }
+	string op4 { get; }
+	string op5 { get; }
+	int process(string op);
+}
+
+[CSharpCallLua]
+public interface ItfEvent
+{
+	string title { get; }
+	string desc { get; }
+	ItfOption option { get;}
+	//ItfOption option { get; }
+}
+
+
 public class MainScript : MonoBehaviour
 {
-
-	[CSharpCallLua]
-	public interface ItfOption
-	{
-		string op1 { get; }
-		string op2 { get; }
-		string op3 { get; }
-		string op4 { get; }
-		string op5 { get; }
-		int process(string op);
-	}
-
-
-
 	// Use this for initialization
 	LuaEnv luaenv = null;
 	// Use this for initialization
 	void Start()
 	{
 		luaenv = new LuaEnv();
+
+		LuaEnv.CustomLoader method = CustomLoaderMethod;
+
+		//添加自定义装载机Loader  
+		luaenv.AddLoader(method);
 		luaenv.DoString("require 'event'");
 
-		Debug.Log("title = " + luaenv.Global.Get<string>("title"));
-		Debug.Log("desc = " + luaenv.Global.Get<string>("desc"));
-		ItfOption d3 = luaenv.Global.Get<ItfOption>("option");
-		Debug.Log("op1 = " + d3.op1);
-		Debug.Log("op2 = " + d3.op2);
-		Debug.Log("op3 = " + d3.op3);
-		Debug.Log("op4 = " + d3.op4);
-		Debug.Log("op4 = " + d3.op4);
-		Debug.Log("process = " + d3.process(d3.op1));
+		ItfEvent eventobj = luaenv.Global.Get<ItfEvent>("event");
+
+		luaenv.DoString("require 'event2'");
+
+		ItfEvent eventobj2 = luaenv.Global.Get<ItfEvent>("event");
+
+		Debug.Log("title = " + eventobj.title);
+		Debug.Log("desc = " + eventobj.desc);
+
+		Debug.Log("op1 = " + eventobj.option.op1);
+		Debug.Log("op2 = " + eventobj.option.op2);
+		Debug.Log("op3 = " + eventobj.option.op3);
+		Debug.Log("op4 = " + eventobj.option.op4);
+		Debug.Log("op5 = " + eventobj.option.op5);
+		Debug.Log("process = " + eventobj.option.process(eventobj.option.op1));
+
+
+
+
+		Debug.Log("title = " + eventobj2.title);
+		Debug.Log("desc = " + eventobj2.desc);
+
+		Debug.Log("op1 = " + eventobj2.option.op1);
+		Debug.Log("op2 = " + eventobj2.option.op2);
+		Debug.Log("op3 = " + eventobj2.option.op3);
+		Debug.Log("op4 = " + eventobj2.option.op4);
+		Debug.Log("op5 = " + eventobj2.option.op5);
+		Debug.Log("process = " + eventobj2.option.process(eventobj2.option.op1));
+
 		//Debug.Log("_G.c = " + luaenv.Global.Get<bool>("c"));
 	}
 	
@@ -53,5 +86,20 @@ public class MainScript : MonoBehaviour
 	void Update () 
 	{
 		
+	}
+
+	private byte[] CustomLoaderMethod(ref string fileName)
+	{
+		//找到指定文件  
+		fileName = Application.streamingAssetsPath + "/native/event/" + fileName.Replace('.', '/') + ".lua";
+		Debug.Log(fileName);
+		if (File.Exists(fileName))
+		{
+			return File.ReadAllBytes(fileName);
+		}
+		else
+		{
+			return null;
+		}
 	}
 }
--- Assets/StreamingAssets/native/event/event.lua
+++ Assets/StreamingAssets/native/event/event.lua
@@ -1,16 +1,21 @@
-event_1={
-	title='title_test'
-	desc='desc_test'
+event={
+	id='EVENT_001',
+	title='title_test',
+	desc='desc_test',
+	
 	option={
 		op1='op1_test',
 		op2='op2_test',
+		
 		process = function(self, op)
-			print(title..' do '..op)
+			print(event.title..' do '..op)
 			local TestClass = CS.TestClass
 			local testobj = TestClass();
-			testobj.a=1
+			testobj.a=3
 			testobj:inc()
 			return testobj.a
 		end
-		}
+		
 	}
+		
+}
